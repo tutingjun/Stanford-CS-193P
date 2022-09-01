e901e6f166c8f3cdcbf8844f25a102816a6853b8
tutingjun <50512161+tutingjun@users.noreply.github.com>
2022-09-01 10:46:52 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by 涂庭鋆 on 2022/9/1.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
